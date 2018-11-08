20471c59cad35eefbdf64b0a0e9f64063b92e0b1
your Name <you@example.com>
2018-11-08 19:38:21 +0530

    create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { TechnologyComponent } from './components/technology/technology.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BreadcrumpComponent } from './components/breadcrump/breadcrump.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutmeComponent,
+    ContactComponent,
+    NavbarComponent,
+    ProjectsComponent,
+    TechnologyComponent,
+    FooterComponent,
+    BreadcrumpComponent
   ],
   imports: [
     BrowserModule,
